319033ab78d6aedb907d3640bbb9d339c646dec4
Sumit <sumityadavcools@gmail.com>
2017-12-06 21:50:47 +0530

    typo fixed for DEBUG_MODE

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -11,10 +11,10 @@ class NeuralNetwork(object):
 			self.bias = [np.round(np.random.randn(i, 1), 2) for i in size_layer[1:]]
 			print(self.bias)
 			# No weight-Input for first(Input) layer and no weight-Output for last(Output) layer
-			self.weight = [np.random.randn(j, i) for i, j in zip(size_layer[:-1], size_layer[1:])]
+			self.weight = [np.round(np.random.randn(j, i), 2) for i, j in zip(size_layer[:-1], size_layer[1:])]
 			print(self.weight)
 		else:
 			self.bias = [np.random.randn(i, 1) for i in size_layer[1:]]
-			self.weight = [np.round(np.random.randn(j, i), 2) for i, j in zip(size_layer[:-1], size_layer[1:])]
+			self.weight = [np.random.randn(j, i) for i, j in zip(size_layer[:-1], size_layer[1:])]
 
 net = NeuralNetwork([1,2,3])
